0195dc2776840afedf4382ffbd1849a1ee5b842c
joshIsCoding <yes_you_do@hotmail.com>
2019-07-20 16:19:40 +0100

    Add basic board setup methods using placeholder Piece class

---
+++ board.rb
@@ -0,0 +1,28 @@
+require_relative "piece.rb"
+
+class Board
+   attr_reader :rows
+   LAYOUT = [:rook, :knight, :bishop, :queen, :king, :bishop, :knight, :rook ]
+   
+   def initialize
+      @rows = Array.new(8) { Array.new(8) }
+      set_pieces
+   end
+
+   def set_pieces
+      (0...8).each do |col|
+         [0,1,6,7].each { |row| add_piece(Piece.new, [col, row])}
+      end
+   end
+
+   def add_piece(piece, pos)
+      self[pos] = piece
+   end
+
+   def []=(pos, value)
+      col, row = pos
+      @rows[row][col] = value
+   end
+
+
+end
