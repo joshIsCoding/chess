7100a21d9e67b1cf33fbc680238379a2a200d87b
joshIsCoding <yes_you_do@hotmail.com>
2019-08-03 18:32:43 +0100

    Refactor Slideable#horizontal_dirs to end loop early if board limits reached

--- slideable.rb
+++ slideable.rb
@@ -4,10 +4,11 @@ module Slideable
    
    def horizontal_dirs
       horizontal_moves = Hash.new { |h, k| h[k] = [] }
-      (1...8).each do |increment|
-         HORIZONTAL_DIRS.each do |dir|
+      HORIZONTAL_DIRS.each do |dir|
+         (1...8).each do |increment|
             new_move = [self.pos[0] + dir[0] * increment,  self.pos[1] + dir[1] * increment ]
-            horizontal_moves[dir].push(new_move) if new_move.min >= 0 && new_move.max < 8
+            break if new_move.min < 0 || new_move.max >= 8
+            horizontal_moves[dir].push(new_move)
             
          end
       end
