4b26ecbd8bc85f5ad0428c7e267b002b8e548729
joshIsCoding <yes_you_do@hotmail.com>
2019-08-03 17:37:25 +0100

    Add Slideable#horizontal_dirs method to produce hash of moves grouped by direction

--- piece.rb
+++ piece.rb
@@ -1,4 +1,8 @@
+require_relative "slideable.rb"
 class Piece
+   include Slideable
+   attr_reader :pos
    def initialize
+      @pos = [4,4]
    end
 end
--- slideable.rb
+++ slideable.rb
@@ -3,6 +3,16 @@ module Slideable
    DIAGONAL_DIRS = [[1,1], [1,-1], [-1, -1], [-1, 1]]
    
    def horizontal_dirs
-
+      horizontal_moves = Hash.new { |h, k| h[k] = [] }
+      (1...8).each do |increment|
+         HORIZONTAL_DIRS.each do |dir|
+            new_move = [self.pos[0] + dir[0] * increment,  self.pos[1] + dir[1] * increment ]
+            horizontal_moves[dir].push(new_move) if new_move.min >= 0 && new_move.max < 8
+            
+         end
+      end
+      horizontal_moves
    end
+
+            
 end
