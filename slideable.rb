a9010a7248250e4765e5c9194b256d1afcbad43d
joshIsCoding <yes_you_do@hotmail.com>
2019-08-05 13:01:51 +0100

    Refactor horizontal and diagonal_dirs methods to use new helper method #grow_unblocked_moves_in_dir

--- slideable.rb
+++ slideable.rb
@@ -3,17 +3,39 @@ module Slideable
    DIAGONAL_DIRS = [[1,1], [1,-1], [-1, -1], [-1, 1]]
    
    def horizontal_dirs
-      horizontal_moves = Hash.new { |h, k| h[k] = [] }
+      horizontal_moves = Hash.new
       HORIZONTAL_DIRS.each do |dir|
-         (1...8).each do |increment|
-            new_move = [self.pos[0] + dir[0] * increment,  self.pos[1] + dir[1] * increment ]
-            break if new_move.min < 0 || new_move.max >= 8
-            horizontal_moves[dir].push(new_move)
-            
-         end
+         unblocked_moves = grow_unblocked_moves_in_dir(dir)
+         next if unblocked_moves.empty?
+         horizontal_moves[dir] = unblocked_moves
       end
       horizontal_moves
    end
+
+   def diagonal_dirs
+      diagonal_moves = Hash.new
+      DIAGONAL_DIRS.each do |dir|
+         unblocked_moves = grow_unblocked_moves_in_dir(dir)
+         next if unblocked_moves.empty?
+         diagonal_moves[dir] = unblocked_moves
+      end
+      diagonal_moves
+   end
+
+   def grow_unblocked_moves_in_dir(dir)
+      dir_moves = []
+      (1...8).each do |increment|
+            new_move = [self.pos[0] + dir[0] * increment,  self.pos[1] + dir[1] * increment ]
+            break if new_move.min < 0 || new_move.max >= 8
+            #break if !self.board[new_move].empty?
+            dir_moves.push(new_move)
+            
+         end
+      dir_moves
+      
+   end
+      
+
 
             
 end
