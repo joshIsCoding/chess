bec32a51ba5e5c651e101f8f77e4c169409970d9
joshIsCoding <yes_you_do@hotmail.com>
2019-08-05 23:15:12 +0100

    Change Piece class to include and test stepable module

--- piece.rb
+++ piece.rb
@@ -1,6 +1,7 @@
 require_relative "slideable.rb"
+require_relative "stepable.rb"
 class Piece
-   include Slideable
+   include Stepable
    attr_reader :pos
    def initialize(pos)
       @pos = pos
