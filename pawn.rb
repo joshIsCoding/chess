5ef44cba357926dc9e3d0859cc6bde4cf963b82f
joshIsCoding <yes_you_do@hotmail.com>
2019-09-02 12:22:02 +0100

    Implement Pawn#side_attacks

--- pawn.rb
+++ pawn.rb
@@ -2,15 +2,7 @@
 class Pawn < Piece
 
    def moves
-      moves = []
-      [1,2].each do |step|
-         break if !at_start_row? && step == 2
-         move = forward_steps(step)
-         
-         break if !@board[move].empty? 
-         moves << move if @board.valid_pos?(move)
-      end
-      return moves
+      forward_steps + side_attacks
    end
 
    def symbol
@@ -23,13 +15,35 @@ class Pawn < Piece
       @color == :white ? 1 : -1
    end
 
-   def forward_steps(count)
+   def forward_steps
       x, y = @pos
-      [x, y + count * forward_dir]
+      steps = []
+      [1,2].each do |step|
+         break if !at_start_row? && step == 2
+         move = [x, y + step * forward_dir]
+         
+         break unless @board.empty?(move)
+         steps << move if @board.valid_pos?(move)
+      end
+      return steps
+   
    end
 
+   def side_attacks
+      x, y = @pos
+      side_attacks = [[x-1, y + forward_dir], [x+1, y + forward_dir]]
+      side_attacks.select do |move|
+         next false unless @board.valid_pos?(move)
+         next false if @board.empty?(move)
+         target = @board[move]
+         target.color != @color
+      end
+   end
+
+
+
    def at_start_row?
-      (@color == :white && @pos.last == 1) || (@color == :black && @pos.last == 0)
+      (@color == :white && @pos.last == 1) || (@color == :black && @pos.last == 6)
    end
 
 end
