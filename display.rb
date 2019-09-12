8fa5918ff51ef8872b9162acf14efd699a679738
joshIsCoding <yes_you_do@hotmail.com>
2019-09-12 23:09:36 +0100

    Refactor significantly to have make_grid/row methods which are printed within #render; separate method determines colour of pieces and adds coloured square backgrounds in checkboard pattern

--- display.rb
+++ display.rb
@@ -4,19 +4,27 @@ require "colorize"
 
 class Display
    COLUMNS = ("a".."h").to_a.freeze
-   ROWS = (1..8).to_a.reverse!.freeze 
+   ROWS = (1..8).to_a.reverse!.freeze
 
-   def initialize(board)
+   def initialize(board = Board.new)
       @cursor = Cursor.new([0,0], board)
       @board = board
-      @spacer = "   "
+   end
+
+   def test
+      @board = Board.new
+      while @board
+         self.render
+         @cursor.get_input
+      end
    end
 
    def render
+      system("clear")
       render_alphabet_labels
       puts
-      @board.rows.each_with_index do |row, i| 
-         render_row(row, i) 
+      build_grid.each do |row|
+         puts row
          puts
       end
       render_alphabet_labels
@@ -24,21 +32,44 @@ class Display
    end
 
    def render_alphabet_labels
-      alphabet_labels = " " + @spacer
-      COLUMNS.each { |letter| alphabet_labels << letter + @spacer }
+      alphabet_labels = "  "
+      COLUMNS.each { |letter| alphabet_labels << " " + letter + "   " }
       puts alphabet_labels
    end
 
-   def render_row(row_array, row_num)
-      row_string = ROWS[row_num].to_s + @spacer
-      row_array.each do |piece| 
-         piece_string = piece.symbol + @spacer
-         row_string << piece_string
+   def build_grid
+      @board.rows.map.with_index do |row, i| 
+         build_row(row, i)
       end
-      row_string << ROWS[row_num].to_s
-
-      puts row_string
    end
+
+   def build_row(row_array, row_num)
+      row_string = ROWS[row_num].to_s + " " 
+      processed_row = row_array.map.with_index do |piece, col_num|
+         pos = [row_num, col_num]
+         piece_char = " " + piece.symbol + " "
+         piece_char.colorize(color_values(pos)) + " "
+      end
+
+      row_string << processed_row.join(" ") + " " + ROWS[row_num].to_s
+
+      row_string
+   end
+
+   def color_values(pos)
+      if pos == @cursor.cursor_pos && @cursor.selected
+         fg, bg = :white, :green
+      elsif pos == @cursor.cursor_pos
+         fg, bg = :black, :light_green
+      elsif (pos.first + pos.last).even?
+         fg, bg = :white, :light_blue
+      else
+         fg, bg = :black, :light_yellow
+      end
+      { :color => fg, :background => bg }
+   end
+
+
 
 
 end
