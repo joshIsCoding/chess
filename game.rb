93a6001e3e3ac77add7f3660aef7775bed8f541a
joshIsCoding <yes_you_do@hotmail.com>
2019-09-21 22:58:19 +0100

    Add basic player movement functionality using HumanPlayer#make_move

--- game.rb
+++ game.rb
@@ -1,6 +1,7 @@
 require_relative "board.rb"
 require_relative "display.rb"
 require_relative "cursor.rb"
+require_relative "humanplayer.rb"
 require "colorize"
 
 class Game
@@ -8,15 +9,15 @@ class Game
       @board = Board.new
       @display = Display.new(@board, true)
 
-      @player_one = HumanPlayer.new(:white)
-      @player_two = AI_player ? AiPlayer.new(:black) : HumanPlayer.new(:black)
+      @player_one = HumanPlayer.new(:white, @display)
+      @player_two = ai_player ? AiPlayer.new(:black) : HumanPlayer.new(:black, @display)
       @current_player = @player_one
    end
 
    def play
       until gameover
          @display.render
-         @current_player.make_move
+         @current_player.make_move(@board)
          switch_player
       end
    end
--- humanplayer.rb
+++ humanplayer.rb
@@ -1,8 +1,31 @@
 class HumanPlayer
-   def initialize(piece_color)
-      @piece_color = piece_color
+   def initialize(piece_color, display)
+      @piece_color, @display = piece_color, display
    end
 
-   def make_move
+   def make_move(board)
+      
+      begin
+         move = []
+         until move.length == 2
+            move << get_pos
+         end
+         board.move_piece(*move)     
+         
+      rescue ArgumentError => move_error
+         puts move_error
+         
+         retry
+         
+      end
+   end
+
+   def get_pos
+      input = nil
+         until input.is_a?(Array)
+            input = @display.cursor.get_input
+            @display.render
+         end
+      input
    end
 end
